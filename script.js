a316c808c3973afefb3fe617a34717f869c800d6
Arthur-Adamov <90524623+Arthur-Adamov@users.noreply.github.com>
2022-05-27 14:56:43 +0300

    game

---
+++ script.js
@@ -0,0 +1,14 @@
+// let nameGuest;
+// nameGuest = prompt('Enter your name');
+// alert(`Ваше имя ${nameGuest}.`);
+
+// let str = 'йцу';
+// console.log(typeof str);
+
+// let num = Number (str);
+// console.log(typeof num)
+// console.log(typeof NaN)
+
+
+
+
