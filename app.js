3755842eb4640e6066cb1658f0402d5269579ed7
Artur Adamov <artur.adamov03340@yandex.ru>
2024-01-19 10:40:15 +0300

    связал локальный репо с удаленным

---
+++ app.js
@@ -0,0 +1,105 @@
+// const firstName = "Artur";
+// // let lastName = "Adamov"; // тип данных string
+// // let age = 31; // тип данных number
+// const isProgrammer = true; // тип данных bolean
+
+
+
+// 2 Мутирование
+//  alert("Ваше имя: " + firstName + ", а ваш возраст: " + age);
+
+// console.log(typeof age);
+// console.log(age.toString());
+
+// let lastName = prompt("Введите фамилию: ");
+// alert(firstName + " " + lastName);
+
+
+
+// 3 Операторы
+
+// let currentYear = 2022;
+//  const birthYear = 1991;
+
+//  const age = currentYear - birthYear;
+
+// // console.log(age);
+
+// const a = 10;
+// const b = 5;
+
+// let c = 32;
+// // c = c + a;
+// c += a;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(currentYear++);
+// console.log(currentYear);
+
+// console.log(c)
+
+
+
+// 4 Типы данных
+
+// const isProgrammer = true;
+// const myName = "Artur";
+// const age = 31;
+// let x
+
+// console.log(typeof isProgrammer);
+// console.log(typeof myName);
+// console.log(typeof age);
+// console.log(typeof x);
+// console.log(typeof null);
+
+
+
+// 5 Приоритет операторов
+
+// const fullAge = 30;
+// const birthYear = 1991;
+// const currentYear = 2022;
+
+// const isFullAge = (currentYear - birthYear) >= fullAge;
+
+// console.log(isFullAge)
+
+
+
+// 6 Условные операторы
+
+// const courseStatus = "pending" // ready, fail, pending
+
+// if (courseStatus === "ready") {
+//     console.log("Курс уже готов и его можно проходить")
+// } else if (courseStatus === "pending") {
+//     console.log("Курс находится в процессе разработки")
+// } else {
+//     console.log("Курс не получился")
+// }
+
+const isReady = true;
+
+// if (isReady) {
+//     console.log("Всё готово!")
+// } else {
+//     console.log("Проект не готов")
+// }
+
+// Тернарное выражение
+isReady ? console.log("Project done") : console.log("Project cancel");
+
+
+// const num1 = 32;
+// const num2 = "32";
+// console.log(num1 == num2);
+// console.log(typeof num1);
+
+
+
+// 7 Булевая логика
+
